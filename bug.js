62f268828864333e2d99eba9f455f79b60d24734
BugHunter-X <askzenai@gmail.com>
2025-02-09 05:10:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+// This is a simplified example, and a real-world scenario might involve more complex nesting.
+<div class="flex">
+  <div class="bg-red-500 p-4">
+    <p>This is a red div.</p>
+    <div class="bg-blue-500 p-2">
+      <p>This is a blue div (nested).</p>
+    </div>
+  </div>
+</div>
+```
